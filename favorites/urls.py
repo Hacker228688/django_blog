07991f7ba6ba85d25b6a8debafd000d7dfa2b2cb
Hacker228688 <hojiakeee@gmail.com>
2024-09-27 12:38:04 +0500

    added new files

--- favorites/urls.py
+++ favorites/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from .views import favorite, add_to_favorite, delete_favorite_item, update_cart_item
 
 app_name = 'favorites'
 
 urlpatterns = [
-    path('', views.favorite, name='favorites'),
-    path('add/<slug:item_slug>/', views.add_to_favorite, name='add_to_favorite'),
-    path('delete/<slug:item_slug>/', views.delete_favorite_item, name='delete_favorite_item'),
-    path('update/', views.update_cart_item, name='update_favorites')
+    path('', favorite, name='favorites'),
+    path('add_favorites/<slug:item_slug>/', add_to_favorite, name='add_to_favorite'),
+    path('delete_favorites/<slug:item_slug>/', delete_favorite_item, name='delete_favorite_item'),
+    path('update_favorites/', update_cart_item, name='update_favorites')
 ]
